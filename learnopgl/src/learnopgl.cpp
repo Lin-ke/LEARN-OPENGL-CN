f069125ff62aace3e0642437593465503f90cad7
yungaroo <17205587660@qq.com>
2022-12-23 12:04:51 +0800

    你好，窗口

--- learnopgl/src/learnopgl.cpp
+++ learnopgl/src/learnopgl.cpp
@@ -1,27 +1,75 @@
-﻿// learnopgl.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿
 
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
+#include <glad/glad.h> // 管理opengl指针
+#include <GLFW/glfw3.h> // 使用opengl并提供一些接口
+void framebuffer_size_callback(GLFWwindow* window, int width, int height); // 当窗口大小改变时调用
+void processInput(GLFWwindow *window);//返回esc按键是否正在被按下
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	glfwInit(); //初始化GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // GLFW配置(hint,value)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // glfw 3.3,核心模式
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);//if use mac-os
+	// 创建窗口对象
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);// 宽，高，标题
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window); // 设置为主线程上下文
+	// 初始化Glad。Glad需要在主线程上下文
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	// 定义视口，一般和窗口大小相同
+
+	glViewport(0, 0, 800, 600);
+	// 注册回调
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	
+	// 渲染循环
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+		// 渲染代码
+		// 清空颜色缓冲
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT); //GL_COLOR_BUFFER_BIT，GL_DEPTH_BUFFER_BIT和GL_STENCIL_BUFFER_BIT
+
+
+
+
+
+
+
+		//前缓冲保存着最终输出的图像，它会在屏幕上显示，而所有的的渲染指令都会在后缓冲上绘制。交换缓冲
+		glfwSwapBuffers(window);
+		//检查有没有触发什么事件
+		glfwPollEvents();
+	}
+	// 释放资源
+	glfwTerminate();
+	
 	return 0;
 }
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
