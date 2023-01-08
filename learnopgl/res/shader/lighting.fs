5a11e7b0e60bc0d95ffccdc2f30c8b5ed136c91d
yungaroo <17205587660@qq.com>
2023-01-08 10:11:14 +0800

    e2-4

--- learnopgl/res/shader/lighting.fs
+++ learnopgl/res/shader/lighting.fs
@@ -1,30 +1,42 @@
 #version 330 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};  // 物体材质
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
 out vec4 FragColor;
 in vec3 Normal; // 顶点法向量
 in vec3 FragPos;	//顶点在世界空间的坐标
 
-uniform vec3 lightPos; // 光源的位置向量
-uniform vec3 objectColor; // 物体颜色
-uniform vec3 lightColor; // 光的颜色
+uniform Material material;
 uniform vec3 viewPos; // 观察者位置
+uniform Light light;
 
 void main()
-{
+{ 
+    // 环境光
+    vec3 ambient = light.ambient * material.ambient;
 
-	// 环境光
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-	// 漫反射
-	vec3 norm = normalize(Normal); 
-	vec3 lightDir = normalize(lightPos - FragPos); // 指向光源的向量
-	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColor;
-	// 镜面反射
-	float specularStrength = 0.5;
-	vec3 viewDir = normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, norm);
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 256);
-	vec3 specular = specularStrength * spec * lightColor;
-    vec3 result = (ambient + diffuse+specular)*objectColor;
+    // 漫反射 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+
+    // 镜面光
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
+
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
--- learnopgl/src/learnopgl.cpp
+++ learnopgl/src/learnopgl.cpp
@@ -151,8 +151,6 @@ int main()
 	// 设置灯立方体的顶点属性（对我们的灯来说仅仅只有位置数据）
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// 光源位置
-	lightingShader.setVec3("lightPos", lightPos);
 
 	// render loop
 	// -----------
@@ -174,9 +172,25 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// 设置光照
 		lightingShader.use();
-		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+		lightingShader.setFloat("material.shininess", 32.0f);
+
+		// 设置光色和各反射所用的光照强度
+		glm::vec3 lightColor;
+		lightColor.x = static_cast<float>(sin(glfwGetTime() * 2.0));
+		lightColor.y = static_cast<float>(sin(glfwGetTime() * 0.7));
+		lightColor.z = static_cast<float>(sin(glfwGetTime() * 1.3));
+		glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); // decrease the influence
+		glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
+		lightingShader.setVec3("light.ambient", ambientColor);
+		lightingShader.setVec3("light.diffuse", diffuseColor);
+		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		
+		// 光源位置
+		lightingShader.setVec3("light.position", lightPos);
+		// 观察者位置
 		lightingShader.setVec3("viewPos", camera.Position);
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
